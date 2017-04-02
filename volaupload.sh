8ab5380e60038ae46c414b64dbb79a859c39c3b2
Szero <singleton@tfwno.gf>
2017-04-02 16:44:24 +0200

    added error handling when people try to use script with no internet access

--- volaupload.sh
+++ volaupload.sh
@@ -26,6 +26,9 @@ SERVER="https://volafile.io"
 COOKIE="/tmp/cuckie"
 RETRIES="3"
 
+#Return non zero value when script gets interrupted with Ctrl+C and remove cookie
+trap failure_exit INT
+
 while true; do
     case "$1" in
         -h | --help) HELP="true" ; shift ;;
@@ -39,7 +42,7 @@ while true; do
                 if [[ "$2" =~ $pe ]]; then
                     ROOM="${BASH_REMATCH[0]}"
                 else
-                    echo -e "\nSorry my dude, but your room ID doesn't match Volafile's format!" >&2
+                    echo -e "\n\033[31mSorry my dude, but your room ID doesn't match Volafile's format!\033[31m\n" >&2
                     exit 2
                 fi
             fi ; shift 2 ;;
@@ -50,8 +53,8 @@ while true; do
         -t | --retries) RETRIES="$2"
             re="^[0-9]$"
             if ! [[ "$RETRIES" =~ $re ]] || [[ "$RETRIES" -lt 0 ]]; then
-                echo -e "\nYou wanted to set negative number of retries or ..." >&2
-                echo -e "... exceeded maximum retry count.\n" >&2
+                echo -e "\n\033[31mYou wanted to set negative number of retries or ..." >&2
+                echo -e "... exceeded maximum retry count.\033[0m\n" >&2
                 exit 3
             fi ; shift 2 ;;
         -w | --watch) WATCHING="true" ; shift ;;
@@ -92,7 +95,7 @@ print_help() {
     echo -e "   You can't retry more than 9 times.\n"
     echo -e "-w, --watch <directory>"
     echo -e "   Makes your script to watch over specific directory. Every file added"
-    echo -e "   to that directory will be uploaded to Volafile. (To exit press Ctrl+Z)\n"
+    echo -e "   to that directory will be uploaded to Volafile. (To exit press Ctrl+C)\n"
     exit 0
 }
 
@@ -105,7 +108,7 @@ proper_exit() { rm -f "$COOKIE"; exit 0; }
 failure_exit() {
     for i in "$@"; do
         echo -e "\033[31m$i\033[0m" >&2
-    done; rm -f "$COOKIE"; exit 1;
+    done; rm -f "$COOKIE"; exit 4;
 }
 #remove cookie on server error to get fresh session for next upload
 skip() {
@@ -113,8 +116,6 @@ skip() {
         echo -e "\033[31m$i\033[0m" >&2
     done; rm -f "$COOKIE"
 }
-#Return non zero value when script jgets interrupted with Ctrl+C and remove cookie
-trap failure_exit INT
 
 extract() {
     _key=$2
@@ -127,6 +128,16 @@ extract() {
         done)
 }
 
+handleErrors() {
+    case $1 in
+        0  ) ;;
+        6  ) failure_exit "\nCheck your interwebz connection, my friendo!\n" ;;
+        101) failure_exit "\nLogin Error: You used wrong login and/or password my dude." \
+                "You wanna login properly, so those sweet volastats can stack up!\n" ;;
+        *  ) failure_exit "\ncURL error of code $1 happend.\n" ;;
+    esac
+}
+
 makeApiCall() {
     method="$1"
     query="$2"
@@ -137,12 +148,12 @@ makeApiCall() {
         if [[ ! -f "$COOKIE" ]]; then
             curl -1 -H "Origin: ${SERVER}" \
             -H "Referer: ${SERVER}" -H "Accept: text/values" \
-            "${SERVER}/rest/login?name=${name}&password=${password}" 2>/dev/null |
+            "${SERVER}/rest/login?name=${name}&password=${password}" 2>/dev/null | \
             cut -d$'\n' -f1 > "$COOKIE"
         fi
         cookie="$(head -qn 1 "$COOKIE")"
         if [[ "$cookie" == "error.code=403" ]]; then
-            return 1
+            return 101
         fi
         curl -1 -b "$cookie" -H "Origin: ${SERVER}" -H "Referer: ${SERVER}" \
             -H "Accept: text/values" "${SERVER}/rest/${method}?${query}" 2>/dev/null
@@ -168,12 +179,7 @@ doUpload() {
         name="Volaphile"
         response=$(makeApiCall getUploadKey "name=$name&room=$room")
     fi
-
-    #shellcheck disable=SC2181
-    if [[ "$?" != "0" ]]; then
-        failure_exit "\nLogin Error: You used wrong login and/or password my dude." \
-                     "You wanna login properly, so those sweet volastats can stack up!\n"
-    fi
+    handleErrors $?
 
     error=$(extract "$response" error)
 
@@ -187,29 +193,29 @@ doUpload() {
 
     # -f option makes curl return error 22 on server responses with code 400 and higher
     if [[ -z "$renamed" ]]; then
-        echo -e "\n\033[32m<^> Uploading \033[1m$file\033[22m to \033[1m$ROOM\033[22m as \033[1m$name\033[22m\n"
+        echo -e "\033[32m<^> Uploading \033[1m$file\033[22m to \033[1m$ROOM\033[22m as \033[1m$name\033[22m\n"
         printf "\033[33m"
         curl --http2 -1 -f -H "Origin: ${SERVER}" -F "file=@\"${file}\"" \
             "${server}/upload?room=${room}&key=${key}" 1>/dev/null
         error="$?"
     else
-        echo -e "\n\033[32m<^> Uploading \033[1m$file\033[22m to \033[1m$ROOM\033[22m as \033[1m$name\033[22m\n"
+        echo -e "\033[32m<^> Uploading \033[1m$file\033[22m to \033[1m$ROOM\033[22m as \033[1m$name\033[22m\n"
         echo -e "-> File renamed to: \033[1m${renamed}\033[22m\n"
         printf "\033[33m"
-        curl --http2 -1 -f -H "Origin: ${SERVER}" -F "file=@\"${file}\";fkilename=\"${renamed}\"" \
+        curl --http2 -1 -f -H "Origin: ${SERVER}" -F "file=@\"${file}\";filename=\"${renamed}\"" \
             "${server}/upload?room=${room}&key=${key}" 1>/dev/null
         error="$?"
         file="$renamed"
     fi
     printf "\033[0m"
-    case "$error" in
-        "0" ) #Replace spaces with %20 so my terminal url finder can see links properly.
+    case $error in
+        0 ) #Replace spaces with %20 so my terminal url finder can see links properly.
               file=$(basename "$file" | sed -r "s/ /%20/g" )
               printf "\n\033[35mVola direct link:\033[0m\n"
               printf "\033[1m%s/get/%s/%s\033[0m\n\n" "$SERVER" "$file_id" "$file" ;;
-        "6" ) failure_exit "\nRoom with ID of \033[1m$ROOM\033[22m doesn't exist! Closing script.\n" ;;
-        "22") skip "\nServer error. Usually caused by gateway timeout.\n" ;;
-        *   ) skip "\nError nr \033[1m${error}\033[22m: Upload failed!\n" ;;
+        6 ) failure_exit "\nRoom with ID of \033[1m$ROOM\033[22m doesn't exist! Closing script.\n" ;;
+        22) skip "\nServer error. Usually caused by gateway timeout.\n" ;;
+        * ) skip "\nError nr \033[1m${error}\033[22m: Upload failed!\n" ;;
     esac
     return $error
 }
@@ -242,6 +248,7 @@ elif [[ -n "$WATCHING" ]] && [[ -n "$ROOM" ]] && [[ $argc == 1 ]]; then
 elif [[ $argc == 2 ]] && [[ -n "$CALL" ]]; then
     set -f ; set -- $TARGETS
     makeApiCall "$1" "$2"
+    handleErrors $?
     proper_exit
 elif [[ $argc -gt 0 ]] && [[ -z "$WATCHING" ]] && [[ -z "$CALL" ]]; then
     set -- $RENAMES
