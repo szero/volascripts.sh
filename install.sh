35c600831f190569cd3f2504e472c5da36129f37
Szero <singleton@tfwno.gf>
2017-04-07 22:12:57 +0200

    make sane progressbars for install script

--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ fi
 
 if ! [[ $(whereis -b youtube-dl | cut -d':' -f2) ]] ; then
     echo -e "\nyoutube-dl wasn't detected, installing ...\n"
-    curl -#L "https://yt-dl.org/downloads/latest/youtube-dl" -o "/usr/local/bin/youtube-dl"
+    curl --progress-bar -L "https://yt-dl.org/downloads/latest/youtube-dl" -o "/usr/local/bin/youtube-dl"
     chmod a+rx /usr/local/bin/youtube-dl
 fi
 
 echo -e "\ninstalling volaupload.sh ...\n"
-curl -#L "https://rawgit.com/Szero/volascripts.sh/master/volaupload.sh" -o "/usr/local/bin/volaupload.sh"
+curl --progress-bar -L "https://rawgit.com/Szero/volascripts.sh/master/volaupload.sh" -o "/usr/local/bin/volaupload.sh"
 chmod a+rx "/usr/local/bin/volaupload.sh"
 
 echo -e "\nInstalling volaupload.sh ...\n"
-curl -#L "https://rawgit.com/Szero/volascripts.sh/master/vid2vola.sh" -o "/usr/local/bin/vid2vola.sh"
+curl --progress-bar -L "https://rawgit.com/Szero/volascripts.sh/master/vid2vola.sh" -o "/usr/local/bin/vid2vola.sh"
 chmod a+rx "/usr/local/bin/vid2vola.sh"
 
 echo -e "\nAll done!"
