4730079fc3919d515dd943a45de3912f5cf1a64d
Szero <singleton@tfwno.gf>
2017-04-07 22:08:30 +0200

    make install script nicer

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
 if [[ $UID -ne 0 ]]; then
-    echo "You have to be root in order to install volascripts.sh"; exit 1
+    echo -e "\nYou have to be root in order to install volascripts.sh"; exit 1
 fi
 
 if ! [[ $(whereis -b youtube-dl | cut -d':' -f2) ]] ; then
-    echo "youtube-dl wasn't detected, installing ..."
-    curl -L "https://yt-dl.org/downloads/latest/youtube-dl" -o "/usr/local/bin/youtube-dl"
+    echo -e "\nyoutube-dl wasn't detected, installing ...\n"
+    curl -#L "https://yt-dl.org/downloads/latest/youtube-dl" -o "/usr/local/bin/youtube-dl"
     chmod a+rx /usr/local/bin/youtube-dl
 fi
 
-curl -L "https://rawgit.com/Szero/volascripts.sh/master/volaupload.sh" -o "/usr/local/bin/volaupload.sh"
+echo -e "\ninstalling volaupload.sh ...\n"
+curl -#L "https://rawgit.com/Szero/volascripts.sh/master/volaupload.sh" -o "/usr/local/bin/volaupload.sh"
 chmod a+rx "/usr/local/bin/volaupload.sh"
 
-curl -L "https://rawgit.com/Szero/volascripts.sh/master/vid2vola.sh" -o "/usr/local/bin/vid2vola.sh"
+echo -e "\nInstalling volaupload.sh ...\n"
+curl -#L "https://rawgit.com/Szero/volascripts.sh/master/vid2vola.sh" -o "/usr/local/bin/vid2vola.sh"
 chmod a+rx "/usr/local/bin/vid2vola.sh"
 
-echo "All done!"
+echo -e "\nAll done!"
